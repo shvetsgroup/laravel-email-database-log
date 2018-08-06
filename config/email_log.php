6e1a085e39bb884dba50055e103e379d0101bd77
dontdo <bruno.dimic@gmail.com>
2018-08-06 18:46:16 +0200

    added missing config for route prefixes

--- config/email_log.php
+++ config/email_log.php
@@ -3,4 +3,5 @@
 return [
     'folder' => env('EMAIL_LOG_ATTACHMENT_FOLDER','email_log_attachments'),
     'access_middleware' => env('EMAIL_LOG_ACCESS_MIDDLEWARE',null),
+    'routes_prefix' => env('EMAIL_LOG_ROUTES_PREFIX',''),
 ];
